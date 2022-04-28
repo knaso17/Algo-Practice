1eab9a2804b758f86aa58105957bd4711aa6f7fc
knaso17 <90427458+knaso17@users.noreply.github.com>
2022-04-28 11:11:49 -0500

    added testing functionality

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Algorithm and Data Structures Practice File
-Writen in JavaScript with Jest for Testing
+
+Written in JavaScript with Jest for Testing
--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,11 @@
 const sum = require('./sum');
+describe("Sum Function Suite", () => {
+  test('Sum adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+  test.skip('Sum returns a number', () => {
+    expect(typeof sum(4, 3)).toBe("number")
+  });
 });
+
