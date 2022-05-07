3e4827932eab4356b86540dadbebd761b7ac22ff
knaso17 <90427458+knaso17@users.noreply.github.com>
2022-05-07 13:15:27 -0500

    tests work with numbers in any order

--- Algos/pairSum.test.js
+++ Algos/pairSum.test.js
@@ -10,13 +10,19 @@ describe("Pair Sum Function Suite", () => {
   });
 
   test('pairSum does\'t use the same number twice', () => {
-    expect(pairSum([5, 11, -1], 10)).toStrictEqual([11, -1]);
+    let expected = [-1, 11];
+    expect(pairSum([5, 11, -1], 10)).toEqual(expect.arrayContaining(expected));
   });
 
   test('assorted test cases with target sum available', () => {
-    expect(pairSum([1, 2, 3, 8, 4], 6)).toStrictEqual([2, 4]);
-    expect(pairSum([5, 10, 20, 30, 35], 40)).toStrictEqual([5, 35]);
-    expect(pairSum([8, 10, 6, 4, -4], 4)).toStrictEqual([8, -4]);
-    expect(pairSum([10, 3, 2, 9, 60, 70], 80)).toStrictEqual([10, 70]);
+    let expected1 = [2, 4];
+    let expected2 = [5, 35];
+    let expected3 = [-4, 8];
+    let expected4 = [10, 70];
+
+    expect(pairSum([1, 2, 3, 8, 4], 6)).toEqual(expect.arrayContaining(expected1));
+    expect(pairSum([5, 10, 20, 30, 35], 40)).toEqual(expect.arrayContaining(expected2));
+    expect(pairSum([8, 10, 6, 4, -4], 4)).toEqual(expect.arrayContaining(expected3));
+    expect(pairSum([10, 3, 9, 60, 70, 71], 80)).toEqual(expect.arrayContaining(expected4));
   });
 });
