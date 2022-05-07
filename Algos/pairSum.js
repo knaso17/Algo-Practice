5762bd757abce70de7456511516d6ed4c8b6ab79
knaso17 <90427458+knaso17@users.noreply.github.com>
2022-05-07 10:17:08 -0500

    add pairSum solution and tests
    The solution uses the brute force method with nested loops. The tests
    are currently order specific and need to be refactored to accept
    numbers in any order.

--- Algos/pairSum.js
+++ Algos/pairSum.js
@@ -1,5 +1,15 @@
-function pairSum(arr, target) {
-  return true;
+function pairSum(array, targetSum) {
+
+	for (let i = 0; i < array.length; i++){
+		for(let y = 0; y < array.length; y++){
+			if( i !== y){
+				if(array[i]+array[y] === targetSum){
+				return [array[i], array[y]]
+			 }
+			}
+		}
+	}
+	return [];
 }
 
 module.exports = pairSum;
--- Algos/pairSum.test.js
+++ Algos/pairSum.test.js
@@ -1,10 +1,22 @@
 const pairSum = require('./pairSum');
 describe("Pair Sum Function Suite", () => {
-  test('Sum adds 1 + 2 to equal 3', () => {
-    expect(pairSum(1, 2)).toBe(3);
+
+  test('pairSum returns an array', () => {
+    expect(Array.isArray(pairSum([4, 3, 8, 1, -1], 0))).toBe(true);
   });
 
-  test('Sum returns a number', () => {
-    expect(typeof pairSum([4, 3, 8, 1, -1], 0)).toBe("string")
+  test('pairSum returns an empty array when the target sum can\'t be found', () => {
+    expect(pairSum([8, 5, 3], 2)).toStrictEqual([]);
+  });
+
+  test('pairSum does\'t use the same number twice', () => {
+    expect(pairSum([5, 11, -1], 10)).toStrictEqual([11, -1]);
+  });
+
+  test('assorted test cases with target sum avaliable', () => {
+    expect(pairSum([5, 11, -1], 10)).toStrictEqual([11, -1]);
+    expect(pairSum([5, 11, -1], 10)).toStrictEqual([11, -1]);
+    expect(pairSum([5, 11, -1], 10)).toStrictEqual([11, -1]);
+    expect(pairSum([5, 11, -1], 10)).toStrictEqual([11, -1]);
   });
 });
