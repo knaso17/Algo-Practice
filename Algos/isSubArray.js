7c06fd90b88fcdc79dc79696df81a13383bffcea
knaso17 <90427458+knaso17@users.noreply.github.com>
2022-05-07 17:05:19 -0500

    completed isSubArray

--- Algos/isSubArray.js
+++ Algos/isSubArray.js
@@ -6,17 +6,27 @@ adjacent.
 */
 
 //My Solution (O(n))
-
+/*
 function isSubArray(array, sequence) {
-	let seqPoint = 0;
-	let arrPoint = 0;
+  let seqPoint = 0;
+  let arrPoint = 0;
 
-	while(seqPoint < sequence.length && arrPoint < array.length){
-		if(sequence[seqPoint] === array[arrPoint]) seqPoint++;
-			arrPoint++;
-	}
-	return seqPoint === sequence.length;
+  while(seqPoint < sequence.length && arrPoint < array.length){
+    if(sequence[seqPoint] === array[arrPoint]) seqPoint++;
+    arrPoint++;
+  }
+  return seqPoint === sequence.length;
+} */
+
+//Alternate Solution (O(n))
+//Loop through the entire array, checking against sequence. If you get a match move pointer, until you reach the end of one array,
+function isSubArray(array, sequence) {
+  let seqPoint = 0;
+  for(const value of array){
+    if (seqPoint === sequence.length) break;
+    if (sequence[seqPoint] === value) seqPoint++;
+  }
+  return seqPoint === sequence.length;
 }
-
 
 module.exports = isSubArray;
