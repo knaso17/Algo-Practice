3cd46b229154f6fd7107258c9da067bb96be5959
knaso17 <90427458+knaso17@users.noreply.github.com>
2022-06-06 14:47:39 -0500

    added tests for linked list: push method

--- DataStructures/singlyLinkedList.test.js
+++ DataStructures/singlyLinkedList.test.js
@@ -1,9 +1,37 @@
 const { Node, SinglyLinkedList } = require("./SinglyLinkedList");
 
-describe("Singly Linked List ", () => {
-  let list = new SinglyLinkedList();
+describe("Singly Linked List", () => {
+  let list;
 
-  test("Push is a valid method", () => {
-    expect(typeof list.push).toBe("function");
+  beforeEach(() => {
+    list = new SinglyLinkedList();
+  });
+
+  describe("Push Method", () => {
+
+    /* What we want to test for push:
+      length goes up by one
+      the value is added to the list
+
+      first value should be both head and tail (next value is null)
+      any subsequent values should be added to the tail
+      test tail and last value of head */
+
+    test("Push is a valid method", () => {
+      expect(typeof list.push).toBe("function");
+    });
+
+    test("Push adds one to the length", () => {
+      list.push("testValue");
+      expect(list.length).toBe(1);
+    })
+
+    test("Push adds one to the length each time", () => {
+      list.push("testValue1");
+      list.push("testValue2");
+      expect(list.length).toBe(2);
+    })
   });
 });
+
+
