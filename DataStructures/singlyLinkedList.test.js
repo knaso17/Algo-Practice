92ed3aaff5badf1a39c7dafc773cc7bc43d339cd
knaso17 <90427458+knaso17@users.noreply.github.com>
2022-06-06 14:59:15 -0500

    completed push tests

--- DataStructures/singlyLinkedList.test.js
+++ DataStructures/singlyLinkedList.test.js
@@ -14,8 +14,7 @@ describe("Singly Linked List", () => {
       the value is added to the list
 
       first value should be both head and tail (next value is null)
-      any subsequent values should be added to the tail
-      test tail and last value of head */
+      subsequent values should be added to the tail and last value of head */
 
     test("Push is a valid method", () => {
       expect(typeof list.push).toBe("function");
@@ -26,11 +25,43 @@ describe("Singly Linked List", () => {
       expect(list.length).toBe(1);
     })
 
+    test("Push adds first value to head and tail", () => {
+      list.push("testValue");
+      expect(list.head.val).toBe("testValue");
+      expect(list.tail.val).toBe("testValue");
+    })
+
     test("Push adds one to the length each time", () => {
       list.push("testValue1");
       list.push("testValue2");
       expect(list.length).toBe(2);
     })
+
+    test("Push adds additional values to tail", () => {
+      list.push("First");
+      list.push("Second");
+      expect(list.head.val).toBe("First");
+      expect(list.tail.val).toBe("Second");
+    })
+
+    test("Push adds additional values in next", () => {
+      list.push("First");
+      list.push("Second");
+      expect(list.head.val).toBe("First");
+      expect(list.head.next.val).toBe("Second");
+    })
+
+    test("Push works for many values", () => {
+      list.push("Hello");
+      list.push("Goodbye");
+      list.push(99);
+      list.push("last value")
+
+      expect(list.head.val).toBe("Hello");
+      expect(list.head.next.val).toBe("Goodbye");
+      expect(list.tail.val).toBe("last value");
+    })
+
   });
 });
 
